ac95169e25aedc2a78907d075f2404c68bab6e4d
Ahmad Hajjar <ahmed_hajjar@hotmail.com>
2023-03-19 22:31:51 +0100

    Search giphy api and show the fetched gifs

--- src/jvmMain/kotlin/me/ahmadhajjar/giphy/Main.kt
+++ src/jvmMain/kotlin/me/ahmadhajjar/giphy/Main.kt
@@ -1,65 +1,15 @@
 package me.ahmadhajjar.giphy
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import App
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.*
-
-
-@Composable
-@Preview
-fun App() {
-    var searchTerm by remember { mutableStateOf(TextFieldValue()) }
-    val focusRequester by remember { mutableStateOf(FocusRequester()) }
-
-    MaterialTheme {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                TextField(
-                    singleLine = true,
-                    value = searchTerm,
-                    onValueChange = {
-                        searchTerm = it
-                    },
-                    placeholder = {
-                        Text(text = "enter search term ...", color = Color.Gray)
-                    },
-                    colors = TextFieldDefaults.textFieldColors(
-                        textColor = Color.White
-                    ),
-                    modifier = Modifier.focusRequester(focusRequester),
-                )
-                Button(onClick = ::handleSearchEvent) {
-                    Text("Search")
-                }
-            }
-        }
-    }
-
-    LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
-    }
-}
-
-fun handleSearchEvent() {
-
-}
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     val windowState = rememberWindowState(
         position = WindowPosition(Alignment.Center),
-        size = DpSize(500.dp, 100.dp),
     )
 
     Window(
